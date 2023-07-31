1fede86e509189c423df4b16b728381a2323bed2
Martynas <martynas.jokubauskis@gmail.com>
2023-07-31 18:29:38 +0300

    Initial commit

---
+++ src/main/java/com/example/Street_manager/StreetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Street_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetManagerApplication.class, args);
+	}
+
+}
